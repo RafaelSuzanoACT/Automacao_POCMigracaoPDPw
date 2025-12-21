d400376b37dd9d17b3024a3bbf459978c25a6ffa
rafael.suzano <rafaelsuzano@hotmail.com>
2025-12-20 23:12:23 -0300

    v3

---
+++ tests/api/Empresa/empresa.spec.js
@@ -0,0 +1,142 @@
+import { test, expect } from '@playwright/test';
+const { Post_Empresa, Get_Empresa, Get_Empresa_Filtro, Delete_Empresa, Get_Empresa_Filtro_CNPJ, Get_Empresa_Verifica_Nome, Get_Empresa_Filtro_Nome,
+  Get_Empresa_Verifica_CNPJ } = require('../../support/commands');
+const empresaFixture = require('../../fixtures/Cadastro_Empresa.json');
+const atualizaEmpresaFixture = require('../../fixtures/Atualiza_Empresa.json');
+let id_empresa
+test.describe('API /Empresas', () => {
+
+  // 🚀 Testes de POST
+  empresaFixture.forEach((payloadEmpresa) => {
+    test(`Enviar POST para ${payloadEmpresa.Cenario}`, async ({ request }, testInfo) => {
+      const expectedStatus = payloadEmpresa.httpcode;
+      const response = await Post_Empresa(request, testInfo.project.use.baseURL, payloadEmpresa);
+
+      const body = await response.json();
+
+  
+      console.log('Projeto:', testInfo.project.name);
+      console.log('BaseURL:', testInfo.project.use.baseURL);
+      console.log('Status Code:', response.status());
+      console.log('Payload enviado:', payloadEmpresa);
+      console.log('Response Body:', body);
+
+      if (expectedStatus !== response.status()) {
+        console.log(`❌ Esperado: ${expectedStatus}, Recebido: ${response.status()}`);
+      } else if (body.id) {
+        console.log('✅ ID retornado:', body.id);
+        id_empresa = body.id
+      }
+      expect(response.status()).toBe(expectedStatus);
+
+    });
+
+
+    
+
+
+  });
+
+  // 🔹 Consulta todas empresas
+  test('Consulta Todas Empresas', async ({ request }, testInfo) => {
+    const response = await Get_Empresa(request, testInfo.project.use.baseURL);
+    const body = await response.json();
+
+    console.log('Projeto:', testInfo.project.name);
+    console.log('BaseURL:', testInfo.project.use.baseURL);
+    console.log('Status Code:', response.status());
+    console.log('Response Body:', body);
+
+    expect(response.status()).toBe(200);
+  });
+
+  // 🔹 Consulta empresa específica por ID
+  test('Consulta Empresa Filtro', async ({ request }, testInfo) => {
+    const response = await Get_Empresa_Filtro(request, testInfo.project.use.baseURL, id_empresa);
+    const body = await response.json();
+
+    console.log('Projeto:', testInfo.project.name);
+    console.log('BaseURL:', testInfo.project.use.baseURL);
+    console.log('Status Code:', response.status());
+    console.log('Response Body:', body);
+
+    expect(response.status()).toBe(200);
+  });
+
+
+  test('Consulta Empresa Filtro CNPJ  ', async ({ request }, testInfo) => {
+    const response = await Get_Empresa_Filtro_CNPJ(request, testInfo.project.use.baseURL, '51147753952888');
+    const body = await response.json();
+
+    console.log('Projeto:', testInfo.project.name);
+    console.log('BaseURL:', testInfo.project.use.baseURL);
+    console.log('Status Code:', response.status());
+    console.log('Response Body:', body);
+
+    expect(response.status()).toBe(200);
+  });
+
+  test('Consulta Empresa Filtro Nome  ', async ({ request }, testInfo) => {
+    const response = await Get_Empresa_Filtro_Nome(request, testInfo.project.use.baseURL, 'Rafael');
+    const body = await response.json();
+
+    console.log('Projeto:', testInfo.project.name);
+    console.log('BaseURL:', testInfo.project.use.baseURL);
+    console.log('Status Code:', response.status());
+    console.log('Response Body:', body);
+
+    expect(response.status()).toBe(200);
+  });
+
+
+  test('Verifica Nome da Empresa  ', async ({ request }, testInfo) => {
+    const response = await Get_Empresa_Verifica_Nome(request, testInfo.project.use.baseURL, 'Rafael', id_empresa);
+    const body = await response.json();
+
+    console.log('Projeto:', testInfo.project.name);
+    console.log('BaseURL:', testInfo.project.use.baseURL);
+    console.log('Status Code:', response.status());
+    console.log('Response Body:', body);
+
+    expect(response.status()).toBe(200);
+  });
+
+  test('Verifica Nome da CNPJ  ', async ({ request }, testInfo) => {
+    const response = await Get_Empresa_Verifica_CNPJ(request, testInfo.project.use.baseURL, '51147753952888');
+    const body = await response.json();
+
+    console.log('Projeto:', testInfo.project.name);
+    console.log('BaseURL:', testInfo.project.use.baseURL);
+    console.log('Status Code:', response.status());
+    console.log('Response Body:', body);
+
+    expect(response.status()).toBe(200);
+  });
+
+
+
+  test('Delete Empresa', async ({ request }, testInfo) => {
+    const response = await Delete_Empresa(request, testInfo.project.use.baseURL, id_empresa);
+
+    console.log('Projeto:', testInfo.project.name);
+    console.log('BaseURL:', testInfo.project.use.baseURL);
+    console.log('Status Code:', response.status());
+
+
+    let body;
+    try {
+      body = await response.json(); // tenta pegar o body
+    } catch (err) {
+      body = 'No content'; // caso não haja body
+    }
+    console.log('Response Body:', body);
+
+    expect(response.status()).toBe(204);
+  });
+
+
+
+
+
+
+});
--- tests/support/commands.js
+++ tests/support/commands.js
@@ -1,6 +1,98 @@
-async function Get_Health(request,BaseURL) {
-  const response = await request.get(BaseURL + '/Health'); // atenção ao case
-  return response; // NÃO faça await aqui
+async function Get_Health(request, BaseURL) {
+  const response = await request.get(BaseURL + '/Health');
+  return response;
 }
 
-module.exports = { Get_Health };
+async function Get_ArquivosDadger(request, BaseURL) {
+  const response = await request.get(BaseURL + '/ArquivosDadger');
+  return response;
+}
+
+async function Get_Empresa(request, BaseURL) {
+  const response = await request.get(BaseURL + '/Empresas');
+  return response;
+}
+
+async function Get_Empresa_Filtro(request, BaseURL, id_empresa) {
+  const response = await request.get(`${BaseURL}/Empresas/${id_empresa}`);
+  return response;
+}
+
+
+async function Get_Empresa_Filtro_CNPJ(request, BaseURL, cnpj) {
+  const response = await request.get(`${BaseURL}/Empresas/cnpj/${cnpj}`);
+  return response;
+}
+
+async function Get_Empresa_Filtro_Nome(request, BaseURL, nome) {
+  const response = await request.get(`${BaseURL}/Empresas/nome/${nome}`);
+  return response;
+}
+
+
+async function Get_Empresa_Verifica_Nome(request, BaseURL, nome) {
+  const response = await request.get(`${BaseURL}/Empresas/verificar-nome/${nome}`);
+  return response;
+}
+
+async function Get_Empresa_Verifica_CNPJ(request, BaseURL, cnpj) {
+  const response = await request.get(`${BaseURL}/Empresas/verificar-cnpj/${cnpj}`);
+  return response;
+}
+ 
+
+
+
+
+
+async function Post_Empresa(request, BaseURL, payload) {
+  const body = { ...payload };
+  delete body.httpcode;
+  delete body.cenario;
+
+  const response = await request.post(`${BaseURL}/Empresas`, {
+    data: body,
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  return response;
+}
+
+
+
+
+
+async function Put_Empresa(request, BaseURL, payload) {
+  const body = { ...payload };
+  delete body.httpcode;
+  delete body.cenario;
+
+  const response = await request.put(`${BaseURL}/Empresas/${id_empresa}`, {
+    data: body,
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  return response;
+}
+
+
+async function Delete_Empresa(request, BaseURL, id_empresa) {
+  const response = await request.delete(`${BaseURL}/Empresas/${id_empresa}`);
+  return response;
+}
+
+
+// ✅ Exportando todas as funções de uma vez
+module.exports = {
+  Get_Health,
+  Get_ArquivosDadger,
+  Get_Empresa,
+  Get_Empresa_Filtro,
+  Post_Empresa,
+  Delete_Empresa,
+  Get_Empresa_Filtro_CNPJ,
+  Get_Empresa_Filtro_Nome,
+  Get_Empresa_Verifica_Nome,
+    Get_Empresa_Verifica_CNPJ,
+    Put_Empresa
+};
